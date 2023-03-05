810081ae43c7f4a332fc0d7d4a41382b9ab66574
Jonathan Hutchinson <jhutchinson531@gmail.com>
2023-03-05 18:34:53 +0000

    Initialise with SolidJs TS template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <main class={styles.App}>
+      <h1>Hello World!</h1>
+    </main>
+  );
+};
+
+export default App;
